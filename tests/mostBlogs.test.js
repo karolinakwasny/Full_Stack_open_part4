84fec4cb83f0935942a62cd1020465d50085aff6
kkwasny <kkwasny@student.42wolfsburg.de>
2024-12-13 17:43:58 +0100

    done till 4.7

--- tests/mostBlogs.test.js
+++ tests/mostBlogs.test.js
@@ -2,25 +2,7 @@ const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
-describe('total likes', () => {
-	const listWithOneBlog = [
-	  {
-		_id: '5a422aa71b54a676234d17f8',
-		title: 'Go To Statement Considered Harmful',
-		author: 'Edsger W. Dijkstra',
-		url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-		likes: 5,
-		__v: 0
-	  }
-	]
-
-	test('when list has only one blog, equals the likes of that', () => {
-	  const result = listHelper.totalLikes(listWithOneBlog)
-	  assert.strictEqual(result, 5)
-	})
-  })
-
-describe('total likes bigger list', () => {
+describe('most blogs', () => {
 	const blogs = [
 		{
 		  _id: "5a422a851b54a676234d17f7",
@@ -72,8 +54,12 @@ describe('total likes bigger list', () => {
 		}
 	  ]
 
-	test('The sum should be 36', () => {
-	  const result = listHelper.totalLikes(blogs)
-	  assert.strictEqual(result, 36)
+	test('It should be "Robert C. Martin", blogs: 3', () => {
+	  const result = listHelper.mostBlogs(blogs)
+	  const expectedResult = {
+		author: "Robert C. Martin",
+		blogs: 3
+	  }
+	  assert.deepStrictEqual(result, expectedResult)
 	})
 })
--- tests/mostLikes.test.js
+++ tests/mostLikes.test.js
@@ -2,25 +2,7 @@ const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
-describe('total likes', () => {
-	const listWithOneBlog = [
-	  {
-		_id: '5a422aa71b54a676234d17f8',
-		title: 'Go To Statement Considered Harmful',
-		author: 'Edsger W. Dijkstra',
-		url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-		likes: 5,
-		__v: 0
-	  }
-	]
-
-	test('when list has only one blog, equals the likes of that', () => {
-	  const result = listHelper.totalLikes(listWithOneBlog)
-	  assert.strictEqual(result, 5)
-	})
-  })
-
-describe('total likes bigger list', () => {
+describe('most likes', () => {
 	const blogs = [
 		{
 		  _id: "5a422a851b54a676234d17f7",
@@ -72,8 +54,12 @@ describe('total likes bigger list', () => {
 		}
 	  ]
 
-	test('The sum should be 36', () => {
-	  const result = listHelper.totalLikes(blogs)
-	  assert.strictEqual(result, 36)
+	test('It should be "Edsger W. Dijkstra", likes: 17', () => {
+	  const result = listHelper.mostLikes(blogs)
+	  const expectedResult = {
+		author: "Edsger W. Dijkstra",
+		likes: 17
+	  }
+	  assert.deepStrictEqual(result, expectedResult)
 	})
 })
