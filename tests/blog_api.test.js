63e228fb2db0f325cb531e7ffd12b8712ad2c8d0
kkwasny <kkwasny@student.42wolfsburg.de>
2025-01-06 22:30:12 +0100

    4.11 done

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -56,6 +56,26 @@ test('a valid blog can be added', async () => {
 
 })
 
+test('blog without likes is not added', async () => {
+	const newBlog = {
+		title: 'a blog that nobody liked',
+		author: 'anonymous',
+		url: 'www.myBlog.com/nolikes',
+	}
+
+	await api
+	  .post('/api/blogs')
+	  .send(newBlog)
+	  .expect(201)
+	  .expect('Content-Type', /application\/json/)
+
+	const blogsAtEnd = await helper.blogsInDb()
+	assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+
+	const likes = blogsAtEnd.map(blog => blog.likes)
+	assert(likes.includes(0))
+  })
+
 after(async () => {
   await mongoose.connection.close()
 })
