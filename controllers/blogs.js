3b07df9b600c96f2178df4b5ab18cc0d6bf8f39c
kkwasny <kkwasny@student.42wolfsburg.de>
2025-03-20 18:23:45 +0100

    4.17 done

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,8 +1,10 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1 })
 
   response.json(blogs)
 })
@@ -13,14 +15,27 @@ blogsRouter.post('/', async (request, response) => {
   if (!body.title || !body.url)
     return response.status(400).json({ error: 'title or url missing' })
 
+  let user;
+
+  if (body.userId)
+    user = await User.findById(body.userId)
+  else
+    user = await User.findOne();
+  if (!user) {
+    return response.status(400).json({ error: 'Invalid or missing userId' });
+  }
+
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
+    user: user.id,
     likes: body.likes !== undefined ? body.likes : 0,
   })
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
   response.status(201).json(savedBlog)
 })
 
