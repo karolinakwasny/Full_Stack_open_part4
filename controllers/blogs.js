bac85366c185aa6ad51992e31d3601ab688e480c
karolinakwasny <kwasnykarolina98@gmail.com>
2025-04-15 18:17:06 +0200

    4.22 done

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -17,13 +17,7 @@ blogsRouter.post('/', async (request, response) => {
   if (!body.title || !body.url)
     return response.status(400).json({ error: 'title or url missing' })
 
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
-  }
-
-  user = await User.findById(decodedToken.id)
-
+  const user = request.user
   if (!user) {
     return response.status(400).json({ error: 'Invalid or missing userId' });
   }
@@ -43,15 +37,16 @@ blogsRouter.post('/', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
+  const user = request.user
+  if (!user) {
+    return response.status(401).json({ error: 'invalid or missing token' })
   }
+
   const blog = await Blog.findById(request.params.id)
   if (!blog) {
     return response.status(404).send({ error: 'Blog not found' })
   }
-  if (blog.user.toString() !== decodedToken.id.toString()){
+  if (blog.user.toString() !== user.id.toString()){
     return response.status(403).send({ error: 'Only the creator can delete this blog' })
   }
   await Blog.findByIdAndDelete(request.params.id)
--- utils/middleware.js
+++ utils/middleware.js
@@ -1,9 +1,20 @@
 const logger = require('./logger')
+const User = require('../models/user')
 
 const tokenExtractor = (request, response, next) => {
   const authorization = request.get('authorization')
   if (authorization && authorization.startsWith('Bearer ')) {
     request.token = authorization.replace('Bearer ', '')
+  }
+  next()
+}
+
+const userExtractor = async (request, response, next) => {
+  if (request.token) {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (decodedToken.id){
+      request.user = await User.findById(decodedToken.id)
+    }
   }
   next()
 }
@@ -38,6 +49,7 @@ const errorHandler = (error, request, response, next) => {
 
 module.exports = {
   tokenExtractor,
+  userExtractor,
   requestLogger,
   unknownEndpoint,
   errorHandler
